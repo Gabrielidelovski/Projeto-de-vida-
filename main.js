005c09b3bf817a6300547f95d78e6fc5f049f24c
Gabrielidelovski <164918814+Gabrielidelovski@users.noreply.github.com>
2025-10-10 14:26:47 -0300

    o

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 .const botoes = document.querySelectorAII(".botao");
 const textos = document.querySelectorAII(".abaconteudo");
+.for (let i = 0,1<botoes.length;i++){
+    botoes[i].onclik = function (){
+        for (let j = 0,j <botoes.lengh;j=++){
+            botoes[j].classList.remove("ativo");
+            ]textos[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
